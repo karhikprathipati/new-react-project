95c47b654c5c9e8b96fe6cb3f91bb6c8d27f030a
karthikpratihpati <karthikprathipati1@gmail.com>
2022-04-22 10:02:56 +0530

    Created Page.js

---
+++ src/Page.js
@@ -0,0 +1,14 @@
+function Page(){
+  return (
+    <div>
+      <h1>Reasons why react is better to learn!</h1>
+      <ul>
+        <li> It is declarative rather than imperative</li>
+        <li> It is a hirable skill </li>
+        <li> It is composable and well maintained</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Page;
